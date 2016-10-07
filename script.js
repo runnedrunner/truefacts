bb3ffd40bb4d6d9247e1456ac3b88bf8d434be91
runnedrunner <auszhou@nuevaschool.org>
2016-10-07 09:03:29 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getQuote() {
-  var both = $('#txt_name').val();
+  var first = $('#first').val();
+  console.log()
+  var last = $('#last').val();
   $.getJSON('https://api.icndb.com/jokes/random?firstName=' + first + '&lastName=' + last, function(data) {
     $('#quote').html(data.value.joke);
     console.log("blah");
   });
 };
 console.log("stuff");
-var first = 'David';
-var last = 'Gaynor';
 getQuote();
 $('#get_quote').click(getQuote);
