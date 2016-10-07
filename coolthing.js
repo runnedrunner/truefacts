ebf944c727bdd54e5b0859275c83d7251b22f0fa
runnedrunner <auszhou@nuevaschool.org>
2016-10-07 08:54:08 -0700

    Classadoo autocommit

--- coolthing.js
+++
@@ -1,9 +0,0 @@
-function getQuote() {
-  $.getJSON('https://api.icndb.com/jokes/random?firstName=Austin&lastName=Zhou', function(data) {
-    $('#quote').html(data.value.joke);
-    console.log("blah")
-  });
-};
-
-getQuote();
-$('#get_quote').click(getQuote);
